9b3d2fa0360df42fd8c40ba2cc19c3023353f7e3
Shakil <mhshakil194@gmail.com>
2024-01-01 23:38:51 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function showMessage() {
+    
+}
+
+
+// Showing Menu
+
+const menuBtn = document.getElementsByClassName("menu_btn")[0];
+
+function showMenu() {
+    menuBtn.classList.toggle("clicked");
+}
