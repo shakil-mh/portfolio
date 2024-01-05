adb3c1126e95767a58ac5e5786c024770da7f4f6
Shakil <mhshakil194@gmail.com>
2024-01-05 16:10:54 +0600

    Mobile menu added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Showing Menu
 
 const menuBtn = document.getElementsByClassName("menu_btn")[0];
+const menu = document.getElementsByTagName("nav")[0];
+
+console.log(menu);
 
 function showMenu() {
     menuBtn.classList.toggle("clicked");
+    menu.classList.toggle("show_menu");
 }
