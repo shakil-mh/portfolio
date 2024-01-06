265a8875f70d1be5af5629f149331eae45aa4040
Shakil <mhshakil194@gmail.com>
2024-01-06 16:38:51 +0600

    Mobile menu active effect added

--- script.js
+++ script.js
@@ -3,9 +3,16 @@
 const menuBtn = document.getElementsByClassName("menu_btn")[0];
 const menu = document.getElementsByTagName("nav")[0];
 
-console.log(menu);
+// console.log(menuBtnBlur);
 
 function showMenu() {
+
+    const menuBtnHover = document.querySelector(".menu_btn > .hover");
+    const img = document.querySelector(".content_img img");
+
     menuBtn.classList.toggle("clicked");
+    menuBtn.classList.toggle("focused");
     menu.classList.toggle("show_menu");
+    menuBtnHover.classList.toggle("focus");
+    img.classList.toggle("img_slide");
 }
